7d0a79daccbfd250398f4b78f0bdd57d1360a7fa
Xiaolin (Stuart) Cheng <a565837019@qq.com>
2022-03-19 11:05:56 +0000

    New File

---
+++ API_read.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+from datetime import date, timedelta
+
+# URL and Auth key for api verification 
+APIKEY = 'asdjkyfejwgadkhsfgjh'
+# Your electricity meter-point’s MPAN
+MPAN = '1200060128968'
+# Your electricity meter’s serial number
+SR = '12L2368778'
+url = 'https://api.octopus.energy/v1/electricity-meter-points/'+MPAN+'/meters/'+SR+'/consumption/'
+
+
+# Set a time period
+period_from = str(date.today() - timedelta(days=1))
+period_to = str(date.today())
+period = '?period_from='+period_from+'T00:00Z&period_to='+period_to+'T00:00Z'
+
+# Request URL
+r = requests.get(url+period, auth=(APIKEY,''))
+
+# Convert to Pandas DataFrame from JSON
+output_dict = r.json()
+dataframe = pd.DataFrame.from_dict(output_dict['results'])
+# Dataframe reversed to add to CSV, newest at the bottom
+dataframe_reversed = dataframe.iloc[::-1]
+
+# Write to CSV
+with open('Consumption.csv','a') as f:
+    dataframe_reversed.to_csv(f, header = False)
+
+
